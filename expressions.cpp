d46bf51cb9ed3b74624c1bc34bfe7ff3075eff82
Ben Townsend <RBT000011@gmail.com>
2022-09-21 18:43:54 -0500

    introducing increment and decrement in basic math expressions

--- expressions.cpp
+++ expressions.cpp
@@ -2,8 +2,10 @@
 
 int main () {
     int x = 10;
-    int y = 3;
-    int z = x + y;
-    std::cout << z;
+    int y = x++;
+    //  x = x + 5; 
+    // int y = 3;
+    // int z = x / y;
+    std::cout << x;
     return 0;
 } 
