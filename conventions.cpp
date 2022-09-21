9e7d720c89783d5d4cca7f6cb619c2417c9c2ba0
Ben Townsend <RBT000011@gmail.com>
2022-09-21 18:34:48 -0500

    notation

--- conventions.cpp
+++ conventions.cpp
@@ -1,4 +1,11 @@
 int main() {
-    int file_size; 
+    int file_size; // Snake Case
+    int FileSize; // Pascal Case
+    int fileSize; // Camel Case
+    int iFileSize; // Hungarian Notation
     return 0; 
 }
+
+// I am following a long with a video tutorial. 
+// This is information I know, but getting used to 
+// typing for code is important not to skip steps :D
