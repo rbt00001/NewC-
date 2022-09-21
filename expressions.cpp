21f5c48fa8ee141952249b4fe386cc1e2f33dc3c
Ben Townsend <RBT000011@gmail.com>
2022-09-21 18:52:38 -0500

    added file for order of operations

--- expressions.cpp
+++ expressions.cpp
@@ -2,10 +2,12 @@
 
 int main () {
     int x = 10;
-    int y = x++;
+    // int y = x++;
+    int z = ++x; 
     //  x = x + 5; 
     // int y = 3;
     // int z = x / y;
-    std::cout << x;
+    // std::cout << x;
+    std::cout << z;
     return 0;
 } 
