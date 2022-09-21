a1833aed7657b37ed5491512921a50f85b0337a1
Ben Townsend <RBT000011@gmail.com>
2022-09-21 18:25:11 -0500

    quick test of const proved

--- constant.cpp
+++ constant.cpp
@@ -1,4 +1,7 @@
 int main() {
-    double pi = 3.14;
+    const double pi = 3.14;
+     // pi = 0;
+     // Placing const in front of the pi, it prevents the number from 
+     // being altered and will throw an error if it is used. 
     return 0;
 }
