bbd0401dca4f250572d971896cad93119ea1a617
Ben Townsend <RBT000011@gmail.com>
2022-09-21 19:11:47 -0500

    expression with a division that required parenthesis

--- orderOfOperations.cpp
+++ orderOfOperations.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 int main () {
-    double x = 1 + 2 *3;
-    std::cout << x; 
+    int x = 80;
+    int y = 3; 
+    double z = (x + 10) / (3 * y);
+    std::cout << z; 
+
+    // double x = 1 + 2 *3;
+    // std::cout << x; 
+
     return 0;
 }
